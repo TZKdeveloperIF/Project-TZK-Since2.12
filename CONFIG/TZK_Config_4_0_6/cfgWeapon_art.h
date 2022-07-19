5d63a39bf40f613b3f2573df2e9f8bf569691b7c
TZKdeveloperIF <IFtheTZK@hotmail.com>
2022-07-20 01:22:49 +0800

    [CONFIG] Shrapnel weapon

--- CONFIG/TZK_Config_4_0_6/cfgWeapon_art.h
+++ CONFIG/TZK_Config_4_0_6/cfgWeapon_art.h
@@ -167,3 +167,76 @@
 	class MineRocket_0800_xj400: MineRocket_0080_xj400 { displayName = "Mine Rocket(800m/s)"; initSpeed = 800; };
 	class MineRocket_0900_xj400: MineRocket_0080_xj400 { displayName = "Mine Rocket(900m/s)"; initSpeed = 900; };
 	class MineRocket_1000_xj400: MineRocket_0080_xj400 { displayName = "Mine Rocket(1000m/s)"; initSpeed = 1000; };
+
+	class Shrapnel_W_xj400: shell125 {
+		displayName = "M483A1";
+		scopeWeapon = 2; scopeMagazine = 0;
+
+		backgroundReload = 1;
+		reloadSound[] = {"Weapons\reload",0.001,1};
+		magazines[] = {
+			"Shrapnel_W_0080_xj400","Shrapnel_W_0120_xj400","Shrapnel_W_0180_xj400","Shrapnel_W_0300_xj400","Shrapnel_W_0400_xj400"
+			,"Shrapnel_W_0500_xj400","Shrapnel_W_0600_xj400","Shrapnel_W_0700_xj400"
+		};
+		magazineReloadTime = 2.5;
+	};
+	class Shrapnel_W_Base_xj400: shell125 {
+		scopeWeapon = 0; scopeMagazine = 2;
+		displayNameMagazine = "M483A1"; shortNameMagazine = "M483A1";
+		count = 16;
+		initSpeed = 500;
+
+		displayName = "M483A1";
+		ammo = "Shrapnel_W_xj400";
+		sound[] = {"\TZK_Sounds_4_0_6\Coc\amos_fire.wss", db+20 ,1};
+		reloadTime = 5;
+		flash = "gunfire";
+		flashSize = 10.000000;
+		dispersion = 0.00055;
+		autoFire = 0;
+		aiRateOfFire = 0.06; aiRateOfFireDistance = 10000;
+	};
+	#define Shrapnel_W(speed) \
+	class Shrapnel_W_##speed##_xj400: Shrapnel_W_Base_xj400 { \
+		initSpeed = ##speed##; displayName = M483A1 (##speed##m/s); \
+	};
+	Shrapnel_W(0080); Shrapnel_W(0120); Shrapnel_W(0180); Shrapnel_W(0300); Shrapnel_W(0400); Shrapnel_W(0500); Shrapnel_W(0600); Shrapnel_W(0700);
+
+	class Shrapnel_E_xj400: shell125 {
+		displayName = "3-O-13";
+		scopeWeapon = 2; scopeMagazine = 0;
+
+		backgroundReload = 1;
+		reloadSound[] = {"\TZK_Sounds_4_0_0\VME\aa1.wav", 0.1, 1};
+		magazines[] = {
+			"Shrapnel_E_0080_xj400","Shrapnel_E_0120_xj400","Shrapnel_E_0180_xj400","Shrapnel_E_0300_xj400","Shrapnel_E_0400_xj400"
+			,"Shrapnel_E_0500_xj400","Shrapnel_E_0600_xj400","Shrapnel_E_0700_xj400"
+		};
+		magazineReloadTime = 2.5;
+
+		opticsZoomMin = 0.04;
+		opticsZoomMax = 0.12;
+		distanceZoomMin = 1500;
+		distanceZoomMax = 80;
+	};
+	class Shrapnel_E_Base_xj400: shell125 {
+		scopeWeapon = 0; scopeMagazine = 2;
+		displayNameMagazine = "3-O-13"; shortNameMagazine = "3-O-13";
+		count = 24;
+		initSpeed = 500;
+
+		displayName = "3-O-13";
+		ammo = "Shrapnel_E_xj400";
+		sound[] = {"\TZK_Sounds_4_0_6\VME\plz89.wss", db+20, 1};
+		reloadTime = 8;
+		flash = "gunfire";
+		flashSize = 10;
+		dispersion = 0.00055;
+		autoFire = 0;
+		aiRateOfFire = 0.06; aiRateOfFireDistance = 10000;
+	};
+	#define Shrapnel_E(speed) \
+	class Shrapnel_E_##speed##_xj400: Shrapnel_E_Base_xj400 { \
+		initSpeed = ##speed##; displayName = 3-O-13 (##speed##m/s); \
+	};
+	Shrapnel_E(0080); Shrapnel_E(0120); Shrapnel_E(0180); Shrapnel_E(0300); Shrapnel_E(0400); Shrapnel_E(0500); Shrapnel_E(0600); Shrapnel_E(0700);
