5d5f709751893462b37a9b96ab3fd9d31a69134d
TZKdeveloperIF <IFtheTZK@hotmail.com>
2023-05-02 15:39:08 +0800

    [CONFIG] Patch for 4.0.6

---
+++ CONFIG/TZK_Patch_4_0_6/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches {
+	class TZK_CONFIG_406_Patch {
+		name = "TZK_CONFIG_406_Patch";
+		author = "IF";
+		units[] = {};
+		requiredAddons[] = {"TZK_CONFIG_406_Release_2"}; // IMPORTANT!
+		requiredVersion = 1.99;
+	};
+};
+
+// EOF
