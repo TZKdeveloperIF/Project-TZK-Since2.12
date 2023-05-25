TZKdeveloperIF/Project-TZK-Since2.12

commit e3085fa0b7b361b9e440fa5aa42873f2785e9921

[func] Bug reported in deploy that main gun can't load howitzer magazines. Try to fix it

MPMissions/General Scripts in Mission/Util/MagazinesReset.sqf
@@ -1,0 +1,8 @@
+private [{_magazinesArray}, {_i}, {_c}];
+
+_magazinesArray = magazinesArray _this;
+{_this removeMagazines _x} forEach magazines _this;
+_i = 0; _c = count _magazinesArray; while {_i < _c} do {
+	_this addMagazinePrecise [_magazinesArray select _i, _magazinesArray select _i + 1];
+	_i = _i + 2;
+};

MPMissions/General Scripts in Mission/Art/AimAndCalcDelay.sqf
@@ -35,8 +35,8 @@ if _ret then {
 	// however no vanilla way to compute this in OFP.
 	if _isVehicle then {
 		if (-1 != ["TOS1_TZK_xj400", "M109A6G_xj400", "PLZ05_TZK_xj400"] find _typeName) then {
-			[_vehicle, 1] exec "\TZK_Scripts_4_0_4\Common\Effect\DeployChange.sqs";
 			_delay = [3, 8] select ("_x == {DeploySwitch_xj400}" count magazines _vehicle == 0);
+			[_vehicle, 1] exec "Util\Deploy\Change.sqs";
 		};
 	};
 };

MPMissions/General Scripts in Mission/Art/Undeploy.sqs
@@ -5,4 +5,4 @@ _typeName = typeOf _this
 ? _typeName != "TOS1_TZK_xj400" && _typeName != "M109A6G_xj400" && _typeName != "PLZ05_TZK_xj400": exit
 ? 0 == {_x == {DeploySwitch_xj400}} count magazines _this: exit
 
-[_this, 0] exec "\TZK_Scripts_4_0_4\Common\Effect\DeployChange.sqs"
+[_this, 0] exec "Util\Deploy\Change.sqs"

MPMissions/General Scripts in Mission/Server/OrderTemp/DeployVehicle.sqs
@@ -26,6 +26,6 @@ _vt = _vehicle call funcGetUnitTypeFromObject
 ? !(_vt in _orderTypes): exit
 
 ; Main Part Of Temporary Order
-[_vehicle, _status] exec "\TZK_Scripts_4_0_4\Common\Effect\DeployChange.sqs"
+[_vehicle, _status] exec "Util\Deploy\Change.sqs"
 
 ; EOF

MPMissions/General Scripts in Mission/Util/Deploy/Change.sqs
@@ -7,8 +7,8 @@ _status = "_x == {DeploySwitch_xj400}" count magazines _vehicle == 0
 ? count _this > 1: _status = _this select 1
 _deploy = [0, 1] select _status
 
-? "M109A6G_xj400" countType [_vehicle] > 0: [_vehicle, _deploy] exec "\TZK_Objects\Scripts\Deploy\M109A6G.sqs"; exit
-? "PLZ05_TZK_xj400" countType [_vehicle] > 0: [_vehicle, _deploy] exec "\TZK_Objects\Scripts\Deploy\PLZ05.sqs"; exit
+? "M109A6G_xj400" countType [_vehicle] > 0: [_vehicle, _deploy] exec "Util\Deploy\M109A6G.sqs"; exit
+? "PLZ05_TZK_xj400" countType [_vehicle] > 0: [_vehicle, _deploy] exec "Util\Deploy\PLZ05.sqs"; exit
 ? "TOS1_TZK_xj400" countType [_vehicle] > 0: [_vehicle, _deploy] exec "\TZK_Config_4_0_4\Scripts\Deploy\TOS1.sqs"; exit
 
 ; EOF

MPMissions/General Scripts in Mission/Util/Deploy/M109A6G.sqs
@@ -1,0 +1,30 @@
+; args: [vehicle, phase]
+
+_vehicle = _this select 0
+_phase = _this select 1
+
+_i = 0 
+_animations = ["backdoor","sidedoor","shovL","shovR","BoxL","BoxR","Clamp"]
+_delays= [0.5, 1, 0, 1, 0, 1, 0]
+
+_NetWorkID = _vehicle call funcGenNetIdStr
+? _phase > 0: _vehicle addMagazine "DeploySwitch_xj400"; _vehicle engineOn false; goto "deploy"
+
+_vehicle removeWeapon "Gun155_xj400"
+? !isNull gunner _vehicle && !local gunner _vehicle: publicExec format [{if (local gunner UnitById %1) then {UnitById %1 removeWeapon "Gun155_xj400"}}, _NetWorkID]
+
+# deploy
+	_vehicle animate [(_animations select _i), _phase]
+	~(_delays select _i)
+	? _i < (count _animations)-1: _i = _i + 1; goto "deploy"
+	
+? _phase == 0: _vehicle removeMagazines "DeploySwitch_xj400"; exit
+
+_vehicle addweapon "Gun155_xj400"
+? !local _vehicle: publicExec format [{if (local UnitById %1) then {UnitById %1 addweapon "Gun155_xj400"}}, _NetWorkID]
+
+; ammo array check. Sometimes bug may occur that weapon can't load magazine
+~ 0.1
+? count (_vehicle ammoArray "Gun155_xj400") > 0: exit
+_vehicle call preprocessFile "Util\MagazinesReset.sqf"
+? !local _vehicle: publicExec format [{if (local UnitById %1) then {UnitById %1 call preprocessFile "Util\MagazinesReset.sqf"}}, _NetWorkID]

MPMissions/General Scripts in Mission/Util/Deploy/PLZ05.sqs
@@ -1,0 +1,30 @@
+; args: [vehicle, phase]
+
+_vehicle = _this select 0
+_phase = _this select 1
+
+_i = 0 
+_animations = ["ZhuChuan"]
+_delays= [3.5]
+
+_NetWorkID = _vehicle call funcGenNetIdStr
+? _phase > 0: _vehicle addMagazine "DeploySwitch_xj400"; _vehicle engineOn false; goto "deploy"
+
+_vehicle removeWeapon "Gun155_xj400"
+? !isNull gunner _vehicle && !local gunner _vehicle: publicExec format [{if (local gunner UnitById %1) then {UnitById %1 removeWeapon "Gun155_xj400"}}, _NetWorkID]
+
+# deploy
+	_vehicle animate [(_animations select _i), _phase]
+	~(_delays select _i)
+	? _i < (count _animations)-1: _i = _i + 1; goto "deploy"
+	
+? _phase == 0: _vehicle removeMagazines "DeploySwitch_xj400"; exit
+
+_vehicle addweapon "Gun155_xj400"
+? !local _vehicle: publicExec format [{if (local UnitById %1) then {UnitById %1 addweapon "Gun155_xj400"}}, _NetWorkID]
+
+; ammo array check. Sometimes bug may occur that weapon can't load magazine
+~ 0.1
+? count (_vehicle ammoArray "Gun155_xj400") > 0: exit
+_vehicle call preprocessFile "Util\MagazinesReset.sqf"
+? !local _vehicle: publicExec format [{if (local UnitById %1) then {UnitById %1 call preprocessFile "Util\MagazinesReset.sqf"}}, _NetWorkID]

MPMissions/General Scripts in Mission/impl/AI_Order_Server.sqf
@@ -28,6 +28,7 @@ _oldPrefix = "\TZK_Scripts_4_0_5\Server\Order\"; _len = sizeofstr _oldPrefix;
 
 orderTempDefs select orderTempRearm set [2, "Server\OrderTemp\Rearm.sqs"];
 orderTempDefs select orderTempChangeAmmo set [2, "Server\OrderTemp\ChangeAmmo.sqs"];
+orderTempDefs select orderTempDeployVehicle set [2, "Server\OrderTemp\DeployVehicle.sqs"];
 comment {
 	orderTempDefs select orderTempMoveUnit set [2, "Server\OrderTemp\MoveUnit.sqs"];
 };

commit ab2535af010d56be234fce1be2bb01b66d8df2ae

[func][bugfix] Fix bug introduced in e3085fa0b7b

MPMissions/General Scripts in Mission/Util/MagazinesReset.sqf
@@ -1,8 +1,13 @@
-private [{_magazinesArray}, {_i}, {_c}];
+private [{_magazinesArray}, {_i}, {_c}, {_ammunition}];
 
 _magazinesArray = magazinesArray _this;
 {_this removeMagazines _x} forEach magazines _this;
 _i = 0; _c = count _magazinesArray; while {_i < _c} do {
-	_this addMagazinePrecise [_magazinesArray select _i, _magazinesArray select _i + 1];
+	_ammunition = _magazinesArray select _i + 1;
+	if (_ammunition > 0) then {
+		_this addMagazinePrecise [_magazinesArray select _i, _ammunition];
+	} else {
+		_this addMagazine (_magazinesArray select _i);
+	};
 	_i = _i + 2;
 };