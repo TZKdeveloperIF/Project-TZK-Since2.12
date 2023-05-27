TZKdeveloperIF/Project-TZK-Since2.12

commit a6fd10d95c5ff17fc5f8d3f5d7342f2c74992c01

[Art] Define art weapons in script without relying on objects' config; realize shrapnel using M109/PLZ05.

MPMissions/General Scripts in Mission/Art/GetAvailableArtWeapon.sqf
@@ -1,0 +1,12 @@
+// arg: vehicle
+// return: weapon name (string)
+
+private [{_weapons},{_weapon}, {_i}, {_c}, {_found}];
+_weapons = weapons _this; _weapon = "";
+
+_i = 0, _c = count _weapons, _found = false; while {_i < _c && not _found} do {
+	if ((_weapons select _i) in ArtVehWeapons) then {_found = true, _weapon = _weapons select _i};
+	_i = _i + 1;
+};
+
+_weapon

MPMissions/General Scripts in Mission/Art/CoreOrder.sqs
@@ -77,7 +77,7 @@ _params = 0, _this = 0
 	_vehicle = vehicle _unit; _typeName = typeOf _vehicle; _script = "Art\FiredEh406.sqs"
 	? _biggerAngle: _heliH = typeOf _target camCreate [0,0,0]
 
-	_bShrapnel = ({"Shrapnel_W_0120_xj400" == _x} count magazines _vehicle > 0)
+	_bShrapnel = false
 	_needAvoidStruct = call preprocessFile "Art\NeedAvoidStruct.sqf"
 	_posT = [-1, -1]; _defenceObject = objNull
 	_tuggedCheckCnt = 0
@@ -155,6 +155,7 @@ _unit doMove _destPos; _unit setUnitPos "UP"
 	_weapon = ""
 	_ret = call preprocessFile "Art\PrepareWeapon.sqf"
 	? not _ret: goto "EOF"
+	_bShrapnel = ("Shrapnel_W_xj400" == _weapon || "Shrapnel_E_xj400" == _weapon)
 # PreCalculate
 	? _unit distance _target > (if _isInfantry then {_range} else {_distLimit}): goto "CheckDistance"
 	? _unit distance _target < _distLimit && _unit distance _target > viewDistance: _unit groupChat "Target is far. Probably missing."

MPMissions/General Scripts in Mission/Art/InitEquipments.sqf
@@ -1,0 +1,12 @@
+// args: none
+// defines weapon and ammo info. They shouldn't relying so much on vehiches' definition in config
+
+ArtVehWeapons = [
+	  "Gun155_xj400"
+	, "Gun120_Grk_xj400", "Gun122_VME_xj400"
+	, "M26Launcher_xj400", "FAE220Launcher_xj400"
+	, "Gun105_xj400", "Gun122_xj400"
+	, "Mortar81_Launcher_xj400", "Mortar82_Launcher_xj400"
+	, "Shrapnel_W_xj400", "Shrapnel_E_xj400"
+	, "MineLayingLauncher_xj400"
+];

MPMissions/General Scripts in Mission/Art/NeedAvoidStruct.sqf
@@ -1,6 +1,5 @@
 "General Area" == _orderType &&
 (
 	(_isVehicle && _type in typesHowitzer) || 
-	_isStruct || 
-	_bShrapnel
+	_isStruct
 )

MPMissions/General Scripts in Mission/Art/PrepareWeapon.sqf
@@ -3,14 +3,19 @@
 _ret = true;
 
 if (_isVehicle || _isStruct) then {
-	_weapon = (_vehicle call funcVehParamWeapons) select 0;
-	if not (_vehicle hasWeapon _weapon) then {
+	_weapon = _vehicle call preprocessFile "Art\GetAvailableArtWeapon.sqf";
+	if ("" == _weapon) then {
 		_msg = "No proper weapon. Order aborted. Rearm me or help me deploy, sir.";
 		_ret = false;
 	};
-	_weapons = (weapons _vehicle) - [_weapon];
-	{_vehicle removeWeapon _x} forEach _weapons;
-	{_vehicle addWeapon _x} forEach _weapons;
+
+	if _ret then {
+		private [{_weapons}];
+		_weapons = weapons _vehicle;
+		[_weapons, [_weapon]] call preprocessFile "Util\ArraySubtract.sqf";
+		{_vehicle removeWeapon _x} forEach _weapons;
+		{_vehicle addWeapon _x} forEach _weapons;
+	};
 };
 
 _ret

MPMissions/General Scripts in Mission/Art/ShrapnelMag.sqf
@@ -1,0 +1,15 @@
+// args: vehicle
+// return: mag name
+
+private [{_mag},{_magArr}, {_found},{_i},{_c}, {_name}];
+
+_mag = "";
+_magArr = magazinesArray _this; _found = false;
+_i = 0; _c = count _magArr; while {_i < _c && not _found} do {
+	_name = _magArr select _i;
+	if (sizeofstr _name == 21) then {
+		if (substr [_name, 0, 8] == "Shrapnel") then {_found = true, _mag = _name};
+	};
+
+	_i = _i + 2;
+};

MPMissions/General Scripts in Mission/Common/Effect/Rocket_FAE220_Track.sqs
@@ -5,7 +5,6 @@ _name = typeOf _rocket
 
 ? _si != si0 && _si != si1: exit
 ? not alive _vehicle: exit
-? -1 != (magazines _vehicle) find "Shrapnel_W_0120_xj400": exit
 
 # loop
 	_pos = getPos _rocket; _vel = velocity _rocket

MPMissions/General Scripts in Mission/Common/Equip/GeneralArtVehicles.sqs
@@ -1,8 +1,11 @@
-; args: [vehicle, type, magazines to be equipped, weapons to be recorded but not equipped]
+; args: [vehicle, type, magazines to be equipped, weapons, whether weapons shall be equipped as well (optional): boolean]
 _v = _this select 0; _t = _this select 1; _m = _this select 2, _w = _this select 3
+_updateWeapons = (if (count _this > 4) then {_this select 4} else {false})
 
 ~ 1
 ? !(local _v) || !(alive _v) : [_v, _w, _m, _t, 1] exec localize {TZK_EQUIPREARMDATA_EDIT}; exit
+
+? _updateWeapons: { _v removeWeapon _x} foreach (weapons _v); {_v addweapon _x} forEach _w
 
 _mags = magazines _v
 

commit 52de6225039df2cb0affdf2cd043a66c58190e10

[Art][bugfix] Fix bug introduced by a6fd10d95c that tank unable to apply art order

MPMissions/General Scripts in Mission/Art/GetAvailableArtWeapon.sqf
@@ -1,12 +1,21 @@
 // arg: vehicle
 // return: weapon name (string)
 
-private [{_weapons},{_weapon}, {_i}, {_c}, {_found}];
-_weapons = weapons _this; _weapon = "";
+private [{_weapon}];
+_weapon = "";
+if ("Tank" == _enum) then {
+	_weapon = (_vehicle call funcVehParamWeapons) select 0;
+	if not (_vehicle hasWeapon _weapon) then {_weapon = ""};
+};
 
-_i = 0, _c = count _weapons, _found = false; while {_i < _c && not _found} do {
-	if ((_weapons select _i) in ArtVehWeapons) then {_found = true, _weapon = _weapons select _i};
-	_i = _i + 1;
+if ("" == _weapon) then {
+	private [{_weapons}, {_i}, {_c}, {_found}];
+	_weapons = weapons _this;
+
+	_i = 0, _c = count _weapons, _found = false; while {_i < _c && not _found} do {
+		if ((_weapons select _i) in ArtVehWeapons) then {_found = true, _weapon = _weapons select _i};
+		_i = _i + 1;
+	};
 };
 
 _weapon