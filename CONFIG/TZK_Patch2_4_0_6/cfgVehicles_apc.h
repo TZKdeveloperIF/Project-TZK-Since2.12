3a54547246670796e3d392c6329f0c555b02f301
TZKdeveloperIF <IFtheTZK@hotmail.com>
2023-06-23 01:49:58 +0800

    [CONFIG] Add APC AA/Town definitions

--- CONFIG/TZK_Patch2_4_0_6/cfgVehicles_apc.h
+++ CONFIG/TZK_Patch2_4_0_6/cfgVehicles_apc.h
@@ -23,6 +23,33 @@
     class M2A2_G_xj400: M2A2_C_xj400 {
         model = "\TZK_Patch2_4_0_6\Model\M2A2_TZK_G.p3d";
     };
+
+	class M2A2_C_AA_xj400: M2A2_C_xj400 {
+		displayName = "M2A2 AA";
+		accuracy = 1000;
+		weapons[] = {Stinger_xj400, MachineGun7_6_xj400};
+		magazines[] = {Stinger_xj400, Stinger_xj400, Stinger_xj400, Stinger_xj400, Stinger_xj400, Stinger_xj400, Stinger_xj400, Stinger_xj400, MachineGun7_6_xj400};
+		threat[] = {1, 0.9, 1};
+		minFireTime = 1;
+	};
+	class M2A2_C_AA_Town_xj400: M2A2_C_AA_xj400 {
+		irScanToEyeFactor = 1.5;
+		sensitivity = 1.6; sensitivityEar = 0.012;
+	};
+	class M2A2_G_AA_xj400: M2A2_G_xj400 {
+		displayName = "M2A2 AA";
+		accuracy = 1000;
+		weapons[] = {Stinger_xj400, MachineGun7_6_xj400};
+		magazines[] = {Stinger_xj400, Stinger_xj400, Stinger_xj400, Stinger_xj400, Stinger_xj400, Stinger_xj400, Stinger_xj400, Stinger_xj400, MachineGun7_6_xj400};
+		threat[] = {1, 0.9, 1};
+		minFireTime = 1;
+	};
+	class M2A2_G_AA_Town_xj400: M2A2_G_AA_xj400 {
+		irScanToEyeFactor = 1.5;
+		sensitivity = 1.6; sensitivityEar = 0.012;
+	};
+
+
     class BMP_xj400: BMP {};
     class BMPAmbul_xj400: BMPAmbul {};
     class BMPAmbul_C_xj400: BMPAmbul_xj400 {
@@ -49,3 +76,28 @@
     class BMP2_G_xj400: BMP2_C_xj400 {
         model = "\TZK_Patch2_4_0_6\Model\BMP2_TZK_G.p3d";
     };
+
+	class BMP2_C_AA_xj400: BMP2_C_xj400 {
+		displayName = "BMP2 AA";
+		accuracy = 1000;
+		weapons[] = {Strela_xj400, MachineGun7_6_xj400};
+		magazines[] = {Strela_xj400, Strela_xj400, Strela_xj400, Strela_xj400, Strela_xj400, Strela_xj400, Strela_xj400, Strela_xj400, MachineGun7_6_xj400};
+		threat[] = {1, 0.9, 1};
+		minFireTime = 1;
+	};
+	class BMP2_C_AA_Town_xj400: BMP2_C_AA_xj400 {
+		irScanToEyeFactor = 1.5;
+		sensitivity = 1.6; sensitivityEar = 0.012;
+	};
+	class BMP2_G_AA_xj400: BMP2_G_xj400 {
+		displayName = "BMP2 AA";
+		accuracy = 1000;
+		weapons[] = {Strela_xj400, MachineGun7_6_xj400};
+		magazines[] = {Strela_xj400, Strela_xj400, Strela_xj400, Strela_xj400, Strela_xj400, Strela_xj400, Strela_xj400, Strela_xj400, MachineGun7_6_xj400};
+		threat[] = {1, 0.9, 1};
+		minFireTime = 1;
+	};
+	class BMP2_G_AA_Town_xj400: BMP2_G_AA_xj400 {
+		irScanToEyeFactor = 1.5;
+		sensitivity = 1.6; sensitivityEar = 0.012;
+	};
