35181447f7ec08b21e56ecaeff19b56f2b96fd37
TZKdeveloperIF <IFtheTZK@hotmail.com>
2024-05-18 16:11:29 +0800

    [CWA-CE] CONFIG and RESOURCE for CWA-CE 2.01 engine uses modified class entry in RESOURCE thus need use CWA files. By now only unitInfoTypes changed in TZK extra attribute in 2.01 CONFIG won't crash CWA-CE but 2.01 provides modified game files. CWA-CE uses origin files thus better use CWA CONFIG as well. All contents are compared and applied TZK/2.01 designs are annotated

---
+++ CONFIG/@TZK_CWA-CE_2.02/bin/cfgAmmo_MineAPE.h
@@ -0,0 +1,20 @@
+// MineAPE provided in 2.01
+
+	class MineAPE: Mine {
+		hit = 15;
+		indirectHit = 18;
+		indirectHitRange = 7;
+		minRange = 15;
+		minRangeProbab = 0.3;
+		midRange = 80;
+		midRangeProbab = 0.55;
+		maxRange = 150;
+		maxRangeProbab = 0;
+		model = "MMN_2_proxy.p3d";
+		defaultMagazine = "MineAPE";
+		activationMass = 15;
+		activationDistance = 10;
+		activationTime = 30;
+		soundHit[] = {"Explosions\expl3",10,1};
+		soundEngine[] = {"",0,1};
+	};
