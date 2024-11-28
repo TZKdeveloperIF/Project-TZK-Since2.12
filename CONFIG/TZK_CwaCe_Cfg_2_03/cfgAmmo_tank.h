e15bc019f2e9f1c7b8c09de41972f146c1c7b73a
TZKdeveloperIF <IFtheTZK@hotmail.com>
2024-11-28 22:51:25 +0800

    [CONFIG] 2.03 config

---
+++ CONFIG/TZK_CwaCe_Cfg_2_03/cfgAmmo_tank.h
@@ -0,0 +1,6 @@
+// todo: highlighted tank shoot, art shell, cannon. All their model is "shell"
+
+// todo: red-orange-yellow colored shell, with proper geometry LOD
+// todo: yellow-blue-green cannon, with proper geometry LOD
+
+// todo: they can't provide expected effects. much worse than drop.
