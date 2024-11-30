3ac572a79360cd2b23574e38458e0ac514c2f10c
TZKdeveloperIF <IFtheTZK@hotmail.com>
2024-11-30 16:06:12 +0800

    [CWA-CE] Rename CHN addon name. Add 2 fonts' fxy origin content

---
+++ CONFIG/CWA_CE_CHN_LANG_2_0_2/config.cpp
@@ -0,0 +1,9 @@
+class CfgPatches {
+	class CWA_CE_CHN_LANG_2_0_2 {
+		name = "CWA_CE_CHN_LANG_2_0_2";
+		author = "IF";
+		units[] = {};
+		requiredAddons[] = {}; // IMPORTANT!
+		requiredVersion = 2.02;
+	};
+};
