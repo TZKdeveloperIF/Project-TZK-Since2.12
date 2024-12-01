f13e4a478f75b5a89fc2795e65b14bfb470b11c8
TZKdeveloperIF <IFtheTZK@hotmail.com>
2024-12-01 17:21:00 +0800

    [CWA-CE] Adjust CHN language fonts. Fix error string table item

--- CONFIG/CWA_CE_CHN_LANG_2_0_2/config.cpp
+++ CONFIG/CWA_CE_CHN_LANG_2_0_2/config.cpp
@@ -1,9 +1,61 @@
 class CfgPatches {
-	class CWA_CE_CHN_LANG_2_0_2 {
-		name = "CWA_CE_CHN_LANG_2_0_2";
+	class CWA_CE_CHN_LANG_2_0_2_02 {
+		name = "CWA_CE_CHN_LANG_2_0_2_02";
 		author = "IF";
 		units[] = {};
 		requiredAddons[] = {}; // IMPORTANT!
 		requiredVersion = 2.02;
+	};
+};
+class CfgFonts {
+	class Korean {
+		TahomaB24 = "hfont16";
+		TahomaB36 = "hfont16";
+		TahomaB48 = "hfont16";
+		CourierNewB64 = "hfont16";
+		Garamond64 = "hfont16";
+		SteelfishB64 = "hfont32";
+		SteelfishB128 = "hfont32_16";
+		AudreysHandB48 = "hfont16";
+		AudreysHandI48 = "hfont16";
+	};
+
+	// 2.01 add Czech/Russian Fonts supporting
+	class Czech {
+		TahomaB24 = "Czech\CZ_TahomaB24";
+		TahomaB36 = "Czech\CZ_TahomaB36";
+		TahomaB48 = "Czech\CZ_TahomaB48";
+		CourierNewB64 = "Czech\CZ_CourierNewB64";
+		AudreysHandB48 = "Czech\CZ_AudreysHandB48";
+		AudreysHandI48 = "Czech\CZ_AudreysHandI48";
+		Garamond64 = "Czech\CZ_Garamond64";
+		SteelfishB128 = "Czech\CZ_SteelfishB128";
+		SteelfishB64 = "Czech\CZ_SteelfishB64";
+		SteelfishB64CE = "Czech\CZ_SteelfishB64CE";
+	};
+	class Russian {
+		TahomaB24 = "Russian\TahomaB24";
+		TahomaB36 = "Russian\TahomaB36";
+		TahomaB48 = "Russian\TahomaB48";
+		CourierNewB64 = "Russian\CourierNewB64";
+		AudreysHandB48 = "Russian\AudreysHandB48";
+		AudreysHandI48 = "Russian\AudreysHandI48";
+		Garamond64 = "Russian\Garamond64";
+		SteelfishB128 = "Russian\SteelfishB128";
+		SteelfishB64 = "Russian\SteelfishB64";
+		SteelfishB64CE = "Russian\SteelfishB64CE";
+	};
+	// Chinese trial
+	class Chinese {
+		TahomaB24 = "Chinese\zeppelin3220";
+		TahomaB36 = "Chinese\zeppelin3220";
+		TahomaB48 = "Chinese\zeppelin3220";
+		CourierNewB64 = "Chinese\zeppelin3220";
+		AudreysHandB48 = "Chinese\Zeppelin33-Italic20";
+		AudreysHandI48 = "Chinese\Zeppelin33-Italic20";
+		Garamond64 = "Chinese\Zeppelin3320";
+		SteelfishB128 = "Chinese\Zeppelin3320";
+		SteelfishB64 = "Chinese\Zeppelin3320";
+		SteelfishB64CE = "Chinese\Zeppelin3320";
 	};
 };
